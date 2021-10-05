b19e0992a778a87464949bbf6fe2551d36cc83d0
황복실 <tlatms0908@gmail.com>
2021-10-05 20:22:14 +0900

    Create: 컴파일용 파일 생성 및 세팅

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+const name = "boksil",
+age = 25,
+gender = "female";
+
+const sayHi = (name: string, age: number, gender: string): string => {
+    return `Hello ${name}, you are ${age}, you are a ${gender}!`; 
+}
+
+console.log(sayHi(name, age, gender));
+ 
+
+export {};
