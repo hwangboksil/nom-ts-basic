97cfb8ce247c8ce712ce8853ca7865e1c1dfb6da
황복실 <tlatms0908@gmail.com>
2021-10-06 12:55:05 +0900

    Modify: interface -> class 변경

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,21 @@
-interface Human {
-    name: string,
-    age: number,
-    gender: string,
-};
+class Human {
+    public name: string;
+    public age: number;
+    public gender: string;
+    constructor(name: string, age: number, gender: string) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+}
 
-const person = {
-    age: 25,
-    name: "boksil",
-    gender: "female"
-};
+const siri = new Human("boksil", 50, "female");
 
 const sayHi = (person: Human): string => {
     return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`; 
 }
 
-console.log(sayHi(person));
+console.log(sayHi(siri));
  
 
 export {};
